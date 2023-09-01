7738221440f18d7d0ae924372ff855ff0e0274cc
Michael Rainer <m.rainer@kreatif.it>
2023-09-01 11:49:26 +0200

    MailSprog Support for cke

--- fragments/mail_sprog/backend/input_tabs.php
+++ fragments/mail_sprog/backend/input_tabs.php
@@ -10,28 +10,39 @@
  */
 
 /** @var rex_sql $sql */
+
+use Cke5\Utils\Cke5Lang;
+
 $sql = $this->getVar('sql', rex_sql::factory());
 /** @var rex_list $list */
-$list   = $this->getVar('list');
-$langs  = array_values(rex_clang::getAll());
-$pager  = $list->getPager();
+$list = $this->getVar('list');
+$langs = array_values(rex_clang::getAll());
+$pager = $list->getPager();
 $itemId = $sql->hasValue('id') ? $sql->getValue('id') : null;
 
 
 $wildcard = $sql->getValue('wildcard');
 $readonly = rex::getUser()->hasPerm('mail_sprog[add]') ? '' : ' readonly="readonly"';
 
+$oldTinyInstalled = (rex_addon::get('tinymce4')->isAvailable());
+// fallback tiny
+$rteClass = $oldTinyInstalled ? 'tiny5-editor' : 'cke5-editor';
+$rteProfile = $oldTinyInstalled ? 'mail-sprog' : 'default';
+
 ?>
 <tr class="editing-tr-heading">
     <td></td>
     <td><?= $itemId ?? '-' ?></td>
     <td colspan="2"></td>
-    <td colspan="2"><input class="form-control" type="text" name="wildcard" value="<?= htmlspecialchars($wildcard) ?>" <?= $readonly ?> /></td>
+    <td colspan="2"><input class="form-control" type="text" name="wildcard"
+                           value="<?= htmlspecialchars($wildcard) ?>" <?= $readonly ?> /></td>
     <td>
-        <button class="btn btn-save btn-block" type="submit" name="func" value="save_wildcard"><?= \rex_i18n::msg('update') ?></button>
+        <button class="btn btn-save btn-block" type="submit" name="func"
+                value="save_wildcard"><?= \rex_i18n::msg('update') ?></button>
     </td>
     <td>
-        <a href="<?= $list->getUrl([$pager->getCursorName() => $pager->getCursor()]) ?>" class="btn btn-primary btn-block" data-pjax="[data-list-container]">
+        <a href="<?= $list->getUrl([$pager->getCursorName() => $pager->getCursor()]) ?>"
+           class="btn btn-primary btn-block" data-pjax="[data-list-container]">
             <?= \rex_i18n::msg('abort') ?>
         </a>
     </td>
@@ -53,22 +64,22 @@ $readonly = rex::getUser()->hasPerm('mail_sprog[add]') ? '' : ' readonly="readon
         <div class="tab-content">
             <?php foreach ($langs as $langIdx => $lang): ?>
                 <?php
-                $_value   = htmlspecialchars($sql->getValue("replace_{$lang->getId()}"));
+                $_value = htmlspecialchars($sql->getValue("replace_{$lang->getId()}"));
                 $_subject = $sql->getValue("subject_{$lang->getId()}");
                 ?>
-                <div role="tabpanel" class="tab-pane  <?= $langIdx == 0 ? 'active' : '' ?>" id="lang-<?= $lang->getId() ?>">
+                <div role="tabpanel" class="tab-pane  <?= $langIdx == 0 ? 'active' : '' ?>"
+                     id="lang-<?= $lang->getId() ?>">
 
                     <div class="form-group">
                         <label class="control-label"><?= rex_i18n::msg('subject') ?></label>
-                        <input type="text" class="form-control" name="subject_<?= $lang->getId() ?>" value="<?= $_subject ?>">
+                        <input type="text" class="form-control" name="subject_<?= $lang->getId() ?>"
+                               value="<?= $_subject ?>">
                     </div>
 
                     <div class="form-group">
                         <label class="control-label"><?= rex_i18n::msg('mail_text') ?></label>
-                        <!-- backwards compatibility (tinymce4 -->
-                        <textarea class="form-control tiny5-editor" name="replace_<?= $lang->getId() ?>"
-                            data-profile="mail-sprog"
-                            data-tiny-mail-sprog><?= $_value ?></textarea>
+                        <textarea class="form-control <?= $rteClass ?>" name="replace_<?= $lang->getId() ?>"
+                                  data-profile="<?= $rteProfile ?>>"><?= $_value ?></textarea>
                     </div>
 
                 </div>
